1a05461f2ffdc46c77cccbddec84e3cd9ecd24b1
raedrdhaounia <raedrdhaounia@gmail.com>
2024-10-17 01:19:25 +0100

    finish svm model

--- data/faq/faq.py
+++ data/faq/faq.py
@@ -1,4 +1,5 @@
 from config.connect import get_db
+from bson.regex import Regex
 
 def get_faqs():
     """Retrieve all FAQs from the collection."""
@@ -10,14 +11,16 @@ def get_faq(question):
     """Retrieve a single FAQ by question."""
     db = get_db()
     collection = db['faq']
-    return collection.find_one({'question': question}, {'_id': 0})
+    res =  list(collection.find({"question": question}, {'_id': 0}))
+    return res
 
 def add_faq(faq):
     """Insert a new FAQ into the collection."""
     db = get_db()
     collection = db['faq']
     result = collection.insert_one(faq)
-    return str(result.inserted_id)
+    faq['_id'] = str(result.inserted_id)
+    return faq
 
 def delete_faq(faq_id):
     """Delete a FAQ by ID."""
@@ -30,5 +33,5 @@ def delete_faqs_by_question():
     """Delete FAQs by question."""
     db = get_db()
     collection = db['faq']
-    result = collection.delete_many()
+    result = collection.delete_many({})
     return result.deleted_count
---
+++ routes/ai/choose_algorithm.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request
+from data.faq.faq import get_faqs
+from config.response import success_response, bad_request_response, server_error_response, not_found_response
+from algorithm.simple_vectorizer_model.response_model import get_prediction as svm_model
+from util.generate_response import translate_text
+chatbot_bp = Blueprint('chatbot', __name__)
+
+algorithm_mapping = {
+    'svm_model': svm_model,
+}
+
+@chatbot_bp.route('/predict', methods=['POST'])
+def predict_response():
+    """
+    Endpoint to get a predicted response from the chatbot model based on user input.
+    
+    Expects a JSON body with a 'message' key. Optionally accepts a 'dataset' key 
+    to provide a custom dataset for the chatbot.
+    """
+    language = request.headers.get('Accept-Language', "en")
+    data = request.get_json()
+    message = data.get('message')
+    if not message:
+        return bad_request_response("message not found")
+    algorithm = request.headers.get('Algorithm')
+
+    model_function = algorithm_mapping.get(algorithm.lower())
+    if not model_function:
+        return not_found_response("model not created")
+
+    try:
+        dataset = get_faqs()
+        response = model_function(translate_text(message, "en"), dataset)
+        data = translate_text(response, language)
+        return success_response("success", data=data)
+    except Exception as e:
+        return server_error_response()
