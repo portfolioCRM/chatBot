2b265da78c225cb7197410c9c1b0027405c7e6fa
Raed Rdhaounia <raedrdhaounia@gmail.com>
2024-10-19 20:07:12 +0100

    Support/swagger documentataion app (#17) (#18)
    * integration flasgger package for swagger ui
    
    * add health care documentation swagger yaml
    
    * create faq yml doc for swagger
    
    * predict route add yml swagger doc
    
    * add flasgger package to the requirement file

--- routes/faq.py
+++ routes/faq.py
@@ -11,13 +11,44 @@ faq_bp = Blueprint('faq', __name__)
 
 @faq_bp.route('', methods=['GET'])
 def get_faq_route():
-    """Get a specific FAQ by question."""
+    """
+    Get a specific FAQ by question.
+    ---
+    tags:
+      - FAQs
+    parameters:
+      - in: query
+        name: question
+        type: string
+        required: true
+        description: "The question to search for."
+      - in: header
+        name: Accept-Language
+        type: string
+        required: true
+        description: "The preferred language for the response."
+    responses:
+      200:
+        description: A list of FAQs
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              question:
+                type: string
+              response:
+                type: string
+      204:
+        description: No content found
+      500:
+        description: Internal server error
+    """
     try:
         question = request.args.get("question")
         accept_language = request.headers.get('Accept-Language', 'en')
         target_language = accept_language.split(',')[0].split('-')[0]
 
-        print(str(question))
         if not question:
             faqs = get_faqs()
         else:
@@ -31,7 +62,42 @@ def get_faq_route():
 
 @faq_bp.route('', methods=['POST'])
 def add_faq_route():
-    """Add a new FAQ."""
+    """
+    Add a new FAQ.
+    ---
+    tags:
+      - FAQs
+    parameters:
+      - in: body
+        name: question
+        required: true
+        description: "The question for the FAQ."
+        schema:
+          type: string
+          example: "What is the meaning of life?"
+
+      - in: body
+        name: response
+        required: true
+        description: "The response for the FAQ."
+        schema:
+          type: string
+          example: "42 is the answer."
+    responses:
+      201:
+        description: FAQ created successfully
+        schema:
+          type: object
+          properties:
+            question:
+              type: string
+            response:
+              type: string
+      400:
+        description: Invalid input
+      500:
+        description: Internal server error
+    """
     data = request.get_json()
     if 'question' in data and 'response' in data:
         try:
@@ -42,9 +108,28 @@ def add_faq_route():
             return server_error_response(message=str(e))
     return bad_request_response(message=faq_message["faq"]["create"]["invalid_input"])
 
+
 @faq_bp.route('/<string:faq_id>', methods=['DELETE'])
 def delete_faq_route(faq_id):
-    """Delete a FAQ by ID."""
+    """
+    Delete a FAQ by ID.
+    ---
+    tags:
+      - FAQs
+    parameters:
+      - in: path
+        name: faq_id
+        type: string
+        required: true
+        description: "The ID of the FAQ to delete."
+    responses:
+      200:
+        description: FAQ deleted successfully
+      404:
+        description: FAQ not found
+      500:
+        description: Internal server error
+    """
     try:
         deleted_count = delete_faq(faq_id)
         if deleted_count:
@@ -53,9 +138,22 @@ def delete_faq_route(faq_id):
     except Exception as e:
         return server_error_response(message=str(e))
 
+
 @faq_bp.route('', methods=['DELETE'])
 def delete_faqs_route():
-    """Delete FAQs by question."""
+    """
+    Delete FAQs by question.
+    ---
+    tags:
+      - FAQs
+    responses:
+      200:
+        description: FAQs deleted successfully
+      404:
+        description: No FAQs found
+      500:
+        description: Internal server error
+    """
     try:
         deleted_count = delete_faqs_by_question()
         if deleted_count > 0 :
@@ -64,10 +162,30 @@ def delete_faqs_route():
     except Exception as e:
         return server_error_response(message=str(e))
 
+
 @faq_bp.route('/import', methods=['POST'])
 def import_faq_route():
-    print(str(request.files))
-    """Import FAQs from a file (JSON or CSV)."""
+    """
+    Import FAQs from a file (JSON or CSV).
+    ---
+    tags:
+      - FAQs
+    consumes:
+      - multipart/form-data
+    parameters:
+      - in: formData
+        name: file
+        type: file
+        required: true
+        description: "The file containing FAQs (JSON or CSV)."
+    responses:
+      200:
+        description: FAQs imported successfully
+      400:
+        description: Invalid input or unsupported file type
+      500:
+        description: Internal server error
+    """
     if 'file' not in request.files:
         return bad_request_response(message="No file part in the request.")
     
